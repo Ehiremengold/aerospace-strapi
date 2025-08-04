cb1f11cf88fde58e944231edd8cb685d35f2a30e
Ehiremen <ehiremengold@gmail.com>
2025-08-05 00:23:00 +0100

    add quarterly report content type, controller, router, and service

---
+++ src/api/quarterly-report/services/quarterly-report.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * quarterly-report service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::quarterly-report.quarterly-report');
