3bf5cedfe49b0b4780da38c4a17b8a340bc3c7b1
Ehiremen <ehiremengold@gmail.com>
2025-08-05 09:57:43 +0100

    remove: delete quarterly report schema, controller, router, service, and related TypeScript definitions

--- src/api/quarterly-report/controllers/quarterly-report.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * quarterly-report controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::quarterly-report.quarterly-report');
