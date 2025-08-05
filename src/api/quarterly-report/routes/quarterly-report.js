e5878692fa5e2f370f4300842e84b66884da8c00
Ehiremen <ehiremengold@gmail.com>
2025-08-05 14:55:36 +0100

    feat: implement quarterly report model, controller, router, and service with attributes

---
+++ src/api/quarterly-report/routes/quarterly-report.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * quarterly-report router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::quarterly-report.quarterly-report');
