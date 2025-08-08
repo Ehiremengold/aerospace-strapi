f97f718e15f3d779cec505e6744c7ffa983e21fb
Ehiremen <ehiremengold@gmail.com>
2025-08-08 03:33:40 +0100

    feat: add rest-cache plugin configuration and update dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,38 @@
-module.exports = () => ({});
+module.exports = {
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "memory",
+        options: {
+          max: 32767,
+          maxAge: 3,
+          updateAgeOnGet: false,
+        },
+      },
+      strategy: {
+        debug: true,
+        maxAge: 3600,
+        updateAgeOnGet: false,
+        contentTypes: [
+          {
+            contentType: "api::application.application",
+            maxAge: 60 * 60 * 100,
+          },
+          { contentType: "api::blog-post.blog-post", maxAge: 60 * 60 * 100 },
+          {
+            contentType: "api::quarterly-report.quarterly-report",
+            maxAge: 60 * 60 * 100,
+          },
+          {
+            contentType: "api::contact-us.contact-us",
+            maxAge: 60 * 60 * 100,
+          },
+          {
+            contentType: "api::job.job",
+            maxAge: 60 * 60 * 100,
+          },
+        ],
+      },
+    },
+  },
+};
